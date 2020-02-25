62b858218048871227709472c2182080539a1c77
Paige Andrews <pandrews25@yahoo.com>
2020-02-24 21:07:31 -0500

    made more components for my pokedex app

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PokeCell from './PokeCell';
+import { pokeClasses } from '../pokeClasses';
 import './styles/PokeList.css';
 
 const PokeList = () => {
+  const cells = pokeClasses.map(pokeClass => <PokeCell key={pokeClass.id} pokeClass={pokeClass}/>)
   return (
     <section className="poke-list">
-
     </section>
   )
 }
